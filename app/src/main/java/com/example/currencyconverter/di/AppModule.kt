058a9879f21256a29b9a9807a5c3c9e45eba01c3
kelechi <oparaiskelechi@gmail.com>
2021-02-17 08:24:51 +0100

    done with the viewmodel and total app

--- app/src/main/java/com/example/currencyconverter/di/AppModule.kt
+++ app/src/main/java/com/example/currencyconverter/di/AppModule.kt
@@ -1,10 +1,16 @@
 package com.example.currencyconverter.di
 
 import com.example.currencyconverter.data.CurrencyApi
+import com.example.currencyconverter.main.DefaultMainRepository
+import com.example.currencyconverter.main.MainRepository
+import com.example.currencyconverter.utils.DispatcherProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.components.ApplicationComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import okhttp3.Dispatcher
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -12,7 +18,7 @@ import javax.inject.Singleton
 
 private const val BASE_URL = "https://api.exchangeratesapi.io"
 @Module
-@InstallIn(ApplicationContext::class)
+@InstallIn(ApplicationComponent::class)
 object AppModule {
 
     @Singleton
@@ -22,4 +28,22 @@ object AppModule {
         .addConverterFactory(GsonConverterFactory.create())
         .build()
         .create(CurrencyApi::class.java)
+
+    @Singleton
+    @Provides
+    fun provideMainRepository(api: CurrencyApi): MainRepository = DefaultMainRepository(api)
+
+    @Singleton
+    @Provides
+    fun provideDispatchers(): DispatcherProvider = object : DispatcherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+
+    }
 }
